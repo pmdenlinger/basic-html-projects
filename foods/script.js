35497c93b0730eab326178d796d27049eeb3631e
Paul Denlinger <pmdenlinger@icloud.com>
2025-02-08 15:56:36 -0800

    Style icons and add transition fo r icons

---
+++ foods/script.js
@@ -0,0 +1,8 @@
+const icons = document.querySelectorAll('.section-1-icons i');
+
+setInterval(() => {
+
+    const icon = document.querySelector('.section-1-icons .change');
+    icon.classList.remove("change");
+
+}, 4000);
